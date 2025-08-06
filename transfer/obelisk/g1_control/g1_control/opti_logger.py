0e696c9f66ab3653294469fb05756d58c25c1f75
Zach Olkin <zach.olkin@gmail.com>
2025-08-06 13:06:52 -0700

    updated the logger to not run in the obelisk stack

--- transfer/obelisk/g1_control/g1_control/opti_logger.py
+++ transfer/obelisk/g1_control/g1_control/opti_logger.py
@@ -8,6 +8,8 @@ from geometry_msgs.msg import PoseStamped
 from obelisk_py.core.sensing import ObeliskSensor
 from obelisk_py.core.utils.ros import spin_obelisk
 from rclpy.executors import SingleThreadedExecutor
+from rclpy.time import Time
+from rclpy.qos import qos_profile_sensor_data
 
 class OptiLogger(ObeliskSensor):
     """Logger for optitrack data."""
@@ -16,12 +18,22 @@ class OptiLogger(ObeliskSensor):
         super().__init__(node_name)
         self._has_sensor_publisher = True   # Hack to get around the Obelisk check.
 
-        self.register_obk_subscription(
-            "sub_optitrack_pose",
-            self.optitrack_pose_callback,  # type: ignore
+        # self.register_obk_subscription(
+        #     "sub_optitrack_pose",
+        #     self.optitrack_pose_callback,  # type: ignore
+        #     PoseStamped,
+        #     key="sub_optitrack_pose",  # key can be specified here or in the config file
+        # )
+
+        # Subscription to OptiTrack mocap data
+        self.subscription = self.create_subscription(
             PoseStamped,
-            key="sub_optitrack_pose",  # key can be specified here or in the config file
-        )
+            '/G1_pelvis/pose',
+            self.optitrack_pose_callback,
+            qos_profile_sensor_data)
+
+        self.start_time = None
+        self.first_rec = False
 
         root = os.environ.get("ROBOT_RL_ROOT", "")
 
@@ -36,17 +48,23 @@ class OptiLogger(ObeliskSensor):
 
     def optitrack_pose_callback(self, msg: PoseStamped) -> None:
         """Callback for the optitrack pose. Log the pose data."""
-        timestamp_sec = msg.header.stamp.sec
+        if not self.first_rec:
+            self.start_time = Time(seconds=msg.header.stamp.sec, nanoseconds=msg.header.stamp.nanosec)
+            self.first_rec = not self.first_rec
+            
+        stamp_time = Time(seconds=msg.header.stamp.sec, nanoseconds=msg.header.stamp.nanosec)
+        time_offset = stamp_time - self.start_time
+        time_float = time_offset.nanoseconds / 1e9
         pos = msg.pose.position
         ori = msg.pose.orientation
         
         self.writer.writerow([
-            timestamp_sec,
+            time_float,
             pos.x, pos.y, pos.z,
             ori.x, ori.y, ori.z, ori.w
         ])
 
-        self.get_logger().info(f'Logged Pose at {timestamp_sec}.')
+        # self.get_logger().info(f'Logged Pose at {time_float}.')
 
     # def compute_state_estimate(self):
     #     pass
